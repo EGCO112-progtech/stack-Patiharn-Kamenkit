fc69d3f792f7c25f7cbd7a71a95683aecb71d58b
Patiharn-Kamenk <122970698+Patiharn-Kamenkit@users.noreply.github.com>
2023-02-25 14:27:54 +0000

    finish

--- main.c
+++ main.c
@@ -1,69 +1,74 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include "node.h"
+//#include "node.h"
 #include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-//void push(NodePtr* top, int x);
-//int pop (NodePtr* top);
-void pop_all(StackPtr s);
-
-int main(int argc, char **argv){
-  
-  int i,N,j;
+int main(int argc, char *argv[]) {
+  NodePtr top = NULL;
+  int i, j, N = 0;
   Stack s;
-  s.top=NULL;
-  s.size=0;
-  
-  for(i=1; i<argc; i++){
+  char x;
+  s.top = NULL;
+  s.size = 0;
+  for (i = 1; i < argc; i++) {
     N=0;
-      for(j=0;j<strlen(argv[i]); j++)
-        {
-          switch(argv[i][j]){
-            case '{' : push(&s,argv[i][j]); break;
-            
-            case '[' : push(&s,argv[i][j]); break;
+    for (j = 0; j < strlen(argv[i]); j++) {
 
-            case '}' : if(pop(&s) != '{') {
-                        if(pop(&s) == '[') N=1; 
-              else N=2;} 
-              else N=0; break;
-                       
-            case ']' : if(pop(&s) != '[') {
-                        if(pop(&s) == '{') N=1; 
-              else N=2;} 
-              else N=0; break;
-          }
-        if(N==1) break;
-        }
-    if(s.size>0){
-      printf("argv %d: incorrect: too many open parenthesis\n",i);
-      pop_all(&s);
+      switch (argv[i][j]) {
+      case '{':
+      case '[':
+        push(&s, (argv[i][j]));
+        break;
+      case '}':
+        x=pop(&s);
+        if (x != '{') {
+          if(x == '[')
+          N = 1;
+          else
+          N=2;}
+
+        break;
+      case ']':
+        x=pop(&s);
+       if (x != '[') {
+          if(x == '{')
+          N = 1;
+          else
+          N=2;}
+        else N=0;
+        break;
+      default:
+        printf("Non of condition\n");
+      }
+      if (N == 1)
+        break;
     }
-    if(N==0) printf("argv %d: Correct\n",i);
-    else if(N==2) printf("argv %d: incorrect: too many close parenthesis\n",i);
-    else printf("argv %d: incorrect: mismatch\n",i);
-    pop_all(&s);
-    
+   
+    if (s.size > 0) {
+      printf("argv %d: Incorrect too many open parathesis\n", i);
+      pop_all(&s);
+    } else if (N == 0)
+      printf("argv %d: Correct\n", i);
+    else if (N == 1)
+      printf("argv %d: Incorrect mismatch \n", i);
+    else
+      printf("argv %d: Incorrect too many close parathesis\n", i);
+      pop_all(&s);
   }
+
+  // pop(&top);
+
+  /*
+  Stack s;
+  printf("Checking the parentheses in argv arguments\n");
+   for(i=1;i<argc;i++){
+      for(j=0;j<strlen(argv[i]);j++){
+         Use stack to help with the parentheses*/
+
+  /*}
+  }
+  */
+
   return 0;
 }
- /*
- Stack s;
- printf("Checking the parentheses in argv arguments\n");
-  for(i=1;i<argc;i++){
-   
-     for(j=0;j<strlen(argv[i]);j++){
-        Use stack to help with the parentheses
-
-
-     }
-
-
-  }
-
-
-
-
-   return 0;
-}*/
